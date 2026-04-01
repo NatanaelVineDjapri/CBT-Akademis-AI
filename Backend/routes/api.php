916e27d72bf1a48c20b31cff8faae9cf0692fe0f
NatanaelVineDjapri <natanaelvinedj@gmail.com>
2026-04-01 07:55:12 +0700

    Update route middleware

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use App\Http\Controllers\AuthController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+// Route public 
 Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
@@ -22,5 +23,33 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/me', [AuthController::class, 'me']);
+    });
+});
+
+//Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Auth
+    Route::prefix('auth')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::put('/update-password', [AuthController::class, 'updatePassword']);
+    });
+
+    //admin only
+    Route::middleware('role:admin')->prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::post('/', [UserController::class, 'store']);
+        Route::get('/{id}', [UserController::class, 'show']);
+        Route::put('/{id}', [UserController::class, 'updateByAdmin']);
+        Route::delete('/{id}', [UserController::class, 'destroy']);
+        Route::post('/import', [UserController::class, 'importBulk']);
+        Route::post('/{id}/reset-password', [UserController::class, 'resetPasswordByAdmin']);
+    });
+
+    ///semua role
+    Route::prefix('profile')->group(function () {
+        Route::put('/', [UserController::class, 'updateProfile']);
+        Route::put('/password', [UserController::class, 'updatePassword']);
     });
 });
